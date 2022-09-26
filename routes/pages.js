cf8d77745c9c51327b9966bd7651538fc1f5858c
Louis Ruocco <louisruocco1@gmail.com>
2022-09-26 15:10:40 +0100

    created link to admin area

--- routes/pages.js
+++ routes/pages.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/static/index.html"))
-})
+});
+
+router.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/static/admin.html"))
+});
 
 module.exports = router;
