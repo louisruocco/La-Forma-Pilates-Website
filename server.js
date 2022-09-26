f013f8bbc7325f0df7576f5f6df70422673844ea
Louis Ruocco <louisruocco1@gmail.com>
2022-09-26 13:58:39 +0100

    server config complete

--- server.js
+++ server.js
@@ -2,8 +2,34 @@ const express = require("express");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const mongoose = require("mongoose");
+const ejs = require("ejs");
+const dotenv = require("dotenv");
 const flash = require("connect-flash");
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening at ${port}`));
+
+dotenv.config({path: "./.env"});
+
+const sessionStore = new MongoDBStore({
+    uri: process.env.DB_URI, 
+    collection: process.env.DB_COLLECTION
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use("/css", express.static("/public/css"));
+app.use("/js", express.static("/public/js"));
+app.use(flash());
+app.set("view engine", "ejs");
+app.use(session({
+    name: process.env.SESS_NAME, 
+    secret: process.env.SECRET, 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 1
+    }
+}))
