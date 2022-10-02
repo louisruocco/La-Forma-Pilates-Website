d293cc7d1812f3c2734ab19384fee8c41ec4918f
Louis Ruocco <louisruocco1@gmail.com>
2022-10-02 18:39:32 +0100

    invoice background color now changes when status is changed from Pending to Complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,15 @@
 const title = document.querySelector(".title");
-const invoiceItem = document.querySelector(".invoice");
+const invoiceItem = document.querySelectorAll(".invoice");
 
 window.addEventListener("load", () => {
     title.classList.add("title-appear");
+});
+
+invoiceItem.forEach(item => {
+    if(item.children[0].textContent === "Pending"){
+        item.style.background = "yellow"
+    } else {
+        item.style.background = "seagreen"
+        item.children[1].children[0].style.color = "black"
+    }
 })
