2e17a20956c8fa06c6271e1cf64e71ef92d54e98
Louis Ruocco <louisruocco1@gmail.com>
2022-10-02 16:03:04 +0100

    invoice background change on status needs work

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,6 @@
 const title = document.querySelector(".title");
 const invoiceItem = document.querySelector(".invoice");
-const invoice = document.querySelector(".invoice span");
 
 window.addEventListener("load", () => {
     title.classList.add("title-appear");
 })
-
-console.log(invoice.textContent);
-
-if(invoice.textContent === "Pending"){
-    invoiceItem.style.background = "rgba(255, 230, 0, 0.644)";
-} else {
-    invoiceItem.style.background = "seagreen"
-}
